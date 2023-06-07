d787f4437353cffc6c35464ae82cca23c29ad22d
KennetAvila28 <83607313+KennetAvila28@users.noreply.github.com>
2023-06-07 14:28:42 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/kennet/wateringsystem/watering_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kennet.wateringsystem.watering_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/environtments/develop.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:watering_system/watering_system_app.dart';
+
+main() {
+  runApp(const WateringSystemApp());
+}
